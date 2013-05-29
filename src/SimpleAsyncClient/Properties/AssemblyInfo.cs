d3de43fa05b964a1232405f01139ac035aab99b7
Jon Canning <jon.canning@gmail.com>
2013-05-29 11:09:19 +0100

    Added files again

--- src/SimpleAsyncClient/Properties/AssemblyInfo.cs
+++ src/SimpleAsyncClient/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -10,16 +9,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Jon Canning")]
 [assembly: AssemblyProduct("SimpleAsyncClient")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("a68bbc0c-182b-40b8-aa85-fbdacb452f4d")]
 
 // Version information for an assembly consists of the following four values:
 //
